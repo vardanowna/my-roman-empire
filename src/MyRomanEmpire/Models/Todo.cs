1be7d0620aa211b5c435113abfc2949196cf1437
anzakaryan <anzakaryan@ozon.ru>
2025-04-22 17:43:34 +0300

    initial commit

---
+++ src/MyRomanEmpire/Models/Todo.cs
@@ -0,0 +1,28 @@
+﻿namespace MyRomanEmpire.Models;
+
+public class Todo
+{
+    public string name;
+    public int id;
+
+    public void newTodo()
+    {
+        Todo todo = new Todo();
+    }
+    
+    public void removeTodo(id)
+    {
+        _todos.remove(_todos[id]);
+    }
+    
+    public void showTodo()
+    {
+        foreach (var todo in _todos)
+            Console.WriteLine(todo);
+    }
+    
+    public void getTodo(id)
+    {
+        Console.WriteLine(_todos[id]);
+    }
+}
---
+++ src/MyRomanEmpire/Program.cs
@@ -0,0 +1,19 @@
+﻿using MyRomanEmpire.Models;
+
+namespace MyRomanEmpire;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("тудусы приветствуют тебя");
+        public readonly List<Todo> _todos = new List<Todo>();
+        
+        while (true)
+        {
+            Console.WriteLine("какой у тебя вопрос?");
+            Console.ReadLine();
+        }
+    }
+}
+
