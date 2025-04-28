aaf20b4834b0b95db8d5f0048bef99f7a1cdf202
anzakaryan <anzakaryan@ozon.ru>
2025-04-28 22:31:25 +0300

    unsolved readonly issue

--- src/MyRomanEmpire/Models/Todo.cs
+++ src/MyRomanEmpire/Models/Todo.cs
@@ -2,27 +2,12 @@
 
 public class Todo
 {
-    public string name;
-    public int id;
-
-    public void newTodo()
-    {
-        Todo todo = new Todo();
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
     
-    public void removeTodo(id)
+    public Todo(int id, string name)
     {
-        _todos.remove(_todos[id]);
-    }
-    
-    public void showTodo()
-    {
-        foreach (var todo in _todos)
-            Console.WriteLine(todo);
-    }
-    
-    public void getTodo(id)
-    {
-        Console.WriteLine(_todos[id]);
+        Id = id;
+        Name = name;
     }
 }
--- src/MyRomanEmpire/Program.cs
+++ src/MyRomanEmpire/Program.cs
@@ -4,15 +4,60 @@ namespace MyRomanEmpire;
 
 public class Program
 {
-    public static void Main()
+    private readonly List<Todo> _todos = new List<Todo>();
+    
+    public static void Main(string[] args)
     {
-        Console.WriteLine("тудусы приветствуют тебя");
-        public readonly List<Todo> _todos = new List<Todo>();
-        
+        System.Console.WriteLine("тудусы приветствуют тебя...");
+        int id = 0;
+
         while (true)
         {
-            Console.WriteLine("какой у тебя вопрос?");
-            Console.ReadLine();
+            System.Console.WriteLine("как поступим с очередным тудусом?....");
+            var command = System.Console.ReadLine();
+
+            if(command == "new")
+            {
+                // делай добавление
+                Console.WriteLine("как назовём засранца?");
+                var name = Console.ReadLine();
+                var todo = new Todo(id, name); //ToDO: проверка на пустой ввод и на неуникальное имя
+                id += 1;
+                Console.WriteLine($"да прибудет с нами новый тудус: {id} {name}...");
+                _todos.Add(todo);
+            }
+            else if(command == "burn")
+            {
+                Console.WriteLine("чей настал черёд?");
+                var id = Console.ReadLine();
+                _todos.RemoveAt(id);
+                Console.WriteLine("прощай, брат...");
+            }
+            else if(command == "edit")
+            {
+                Console.WriteLine("время переименовать тудус");
+                Console.WriteLine("чей настал черёд?");
+                var id = Console.ReadLine();
+                var name = Console.ReadLine();
+                // обратиться к тудусу и сделать set нового имени?
+                Console.WriteLine("прощай, брат...");
+            }
+            else if(command == "all")
+            {
+                foreach (var todo in _todos)
+                    Console.WriteLine(todo);
+                Console.WriteLine("тудусов много не бывает...");
+            }
+            else if(command == "get")
+            {
+                Console.WriteLine("чей настал черёд?");
+                var id = Console.ReadLine();
+                // обратиться к тудусу, сделать get его имени и вывести
+            }
+            else
+            {
+                Console.WriteLine("я не понимаю тебя, брат...");
+            }
         }
     }
 }
