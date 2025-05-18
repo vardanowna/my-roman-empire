67b12a8af342760d4a23f1404222398679f2de56
anzakaryan <anzakaryan@ozon.ru>
2025-05-18 22:58:36 +0300

    Пофиксила сохранение в файл

--- src/MyRomanEmpire/Repositories/TodoRepository.cs
+++ src/MyRomanEmpire/Repositories/TodoRepository.cs
@@ -9,7 +9,7 @@ public class TodoRepository
     private readonly List<Todo> _todos = new List<Todo>();
     private int id = 1;
     public string localPath = "C:\\zakarian\\pet_projects\\my-roman-empire\\files\\";
-    string localDate = DateTime.Now.ToString(new CultureInfo("en-GB")).Replace(" ", "_").Replace(":", "_").Replace("\\", "_");
+    string localDate = DateTime.Now.ToString(new CultureInfo("en-GB")).Replace(" ", "_").Replace(":", "_").Replace("/", "_");
 
     // Create name => id
     public int Create(Todo todo)
@@ -81,10 +81,7 @@ public class TodoRepository
         string fileName = "list_of_todos_" + localDate + ".txt";
         string fullPath = localPath + fileName;
 
-        await using (var fs = File.Create(fullPath))
-        {
-            
-        }
+        File.Create(fullPath).Close();
 
         await using (StreamWriter writer = new StreamWriter(fullPath, true))
         {
